442daa2ebf529339f200b320125c0d1f2e6eb029
zm17462 <zm17462@josai.ac.jp>
2018-12-16 20:49:43 -0500

    message

---
+++ ex06-12.js
@@ -0,0 +1,8 @@
+function ThreeSecChange() {
+  
+  setInterval("imgchange12()",3000); }
+
+
+function imgchange12() {
+  document.getElementById('logo2').src = './image/bulbon.gif';
+}
---
+++ ex06-13.js
@@ -0,0 +1,26 @@
+function ChangeImageAndBack(){
+
+  
+imgback();
+
+ThreeSecChange2();
+
+}
+
+function ThreeSecChange2(){
+   setTimeout("imgchange2()",3000);
+}
+ 
+ 
+function imgchange2(){
+  document.getElementById('img13').src = './image/ju.png';
+  }
+  
+  
+  
+  
+  
+  function imgback() {
+   document.getElementById('img13').src = './image/bulbon.gif';
+   }
+   
